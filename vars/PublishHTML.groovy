373cda11a7e25a1dd4179dd984b6908906e4d10c
houdini91 <mdstrauss91@gmail.com>
2021-05-29 18:07:51 +0300

    test

--- vars/PublishHTML.groovy
+++ vars/PublishHTML.groovy
@@ -29,3 +29,4 @@ def PublishHTML() {
 def call(args) {
     PublishHTML()
 }
+
--- vars/RunInVirtualEnv2.groovy
+++ vars/RunInVirtualEnv2.groovy
@@ -3,7 +3,7 @@ import jenkins.pipeline.lib.Constants
 
 def RunInVirtualEnv2(String name, String command) {
     sh script:'''#!/bin/bash
-    source ''' + name + '''/bin/activate && ''' + command, label: "VIRTUALENV: ${script}"
+    source ''' + name + '''/bin/activate && ''' + command, label: "VIRTUALENV 2: ${script}"
 }
 
 def call(String name = "venv", String command) {
