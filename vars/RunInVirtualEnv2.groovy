34c2fb7df7b1ab7f66147a8ad55d625e3735b0c3
houdini91 <mdstrauss91@gmail.com>
2021-05-29 18:09:28 +0300

    test

--- vars/RunInVirtualEnv2.groovy
+++ vars/RunInVirtualEnv2.groovy
@@ -1,11 +1,15 @@
 #!groovy
 import jenkins.pipeline.lib.Constants
 
-def RunInVirtualEnv2(String name, String command) {
-    sh script:'''#!/bin/bash
-    source ''' + name + '''/bin/activate && ''' + command, label: "VIRTUALENV 2: ${script}"
-}
+// def RunInVirtualEnv2(String name, String command) {
+//     sh script:'''#!/bin/bash
+//     source ''' + name + '''/bin/activate && ''' + command, label: "VIRTUALENV 2: ${script}"
+// }
 
-def call(String name = "venv", String command) {
-    RunInVirtualEnv2(name, command)
+// def call(String name = "venv", String command) {
+//     RunInVirtualEnv2(name, command)
+// }
+
+def call(String name = "venv") {
+    sh 'echo RunInVirutalENv222'
 }
