a4ddec7df3d5b020e0e1faed3c950c604c5db576
houdini91 <mdstrauss91@gmail.com>
2021-05-29 18:15:37 +0300

    test

--- vars/InVirtualEnv.groovy
+++ vars/InVirtualEnv.groovy
@@ -1,14 +1,14 @@
 #!groovy
 import jenkins.pipeline.lib.Constants
 
-def InVirtualEnv(String name, String command) {
+def inVirtualEnv(String name, String command) {
     sh script:'''#!/bin/bash
     source ''' + name + '''/bin/activate && ''' + command, label: "VIRTUALENV 2: ${script}"
 }
 
 def call(String name = "venv", String command="python --version") {
-    // InVirtualEnv(name, command)
      echo "Sampling $name $command"
+    inVirtualEnv(name, command)
 }
 
 // def call(String name = "venv") {
