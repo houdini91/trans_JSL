09ea3f52295cf74672f7de04b85cf22224a8e7c5
houdini91 <mdstrauss91@gmail.com>
2021-05-29 18:11:48 +0300

    test

---
+++ vars/InVirtualEnv.groovy
@@ -0,0 +1,15 @@
+#!groovy
+import jenkins.pipeline.lib.Constants
+
+def InVirtualEnv(String name, String command) {
+    sh script:'''#!/bin/bash
+    source ''' + name + '''/bin/activate && ''' + command, label: "VIRTUALENV 2: ${script}"
+}
+
+def call(String name = "venv", String command="python --version") {
+    InVirtualEnv(name, command)
+}
+
+// def call(String name = "venv") {
+//     sh 'echo RunInVirutalENv222'
+// }
