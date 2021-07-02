71e4deb9721abf6f531851f64ef50e37dd5d788b
houdini91 <mdstrauss91@gmail.com>
2021-07-02 12:28:57 +0300

    Add create env

--- vars/CreateVirtualEnv.groovy
+++ vars/CreateVirtualEnv.groovy
@@ -3,6 +3,10 @@ import jenkins.pipeline.lib.Constants
 
 def createPython3VirtualEnv(String name) {
     sh "echo GOT NAME $name"
+    sh "echo GOT NAME ${name}"
+    sh """echo GOT NAME $name"""
+    sh """echo GOT NAME ${name}"""
+
     sh(script: """python3 -m venv ${name}""",returnStdout: true)
     // sh "python3 -m venv ${name}"
 }
