3ecace15f0165305decf35368d3dba6b14888710
houdini91 <mdstrauss91@gmail.com>
2021-07-02 13:11:01 +0300

    Add create env

--- vars/InVirtualEnv.groovy
+++ vars/InVirtualEnv.groovy
@@ -2,12 +2,11 @@
 import jenkins.pipeline.lib.Constants
 
 def InVirtualEnv(String name = "venv", String command = "python --version") {
-    echo "Sampling2 $name $command"
+    echo "InVirtualEnv $name $command"
     sh(script:'''#!/bin/bash
     source ''' + name + '''/bin/activate && ''' + command, label: "VIRTUALENV: ${command}")
 }
 
 def call(String name="venv", String command="python --version") {
-    echo "Sampling22 $name $command"
     InVirtualEnv(name, command)
 }
